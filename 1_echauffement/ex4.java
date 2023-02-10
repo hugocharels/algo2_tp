f1f576755db9290d14d9ea058996f213b0d3f68c
Hugo <93338995+hugocharels@users.noreply.github.com>
2023-02-10 17:58:57 +0000

    uwu

--- 1_echauffement/ex4.java
+++ 1_echauffement/ex4.java
@@ -1,10 +1,14 @@
 import java.util.Arrays;
 
 class Ex4 {
-	
+
+	private static int findMostOccurrence(int[] array) {
+		Arrays.sort(array);
+		return array[array.length / 2];
+	}
+
 	public static void main(String[] args) {
 		int[] array = new int[]{5, 10, 5, 5, 20, 12, 8, 5, 5};
-		Arrays.sort(array);
-		System.out.println(array[array.length / 2]);
+		System.out.println(findMostOccurrence(array));
 	}
 }
--- 1_echauffement/ex5.java
+++ 1_echauffement/ex5.java
@@ -1,8 +1,22 @@
 class Ex5 {
 	
-	public static void main(String[] args) {
-		
-		
+	private static int findMostOccurrence(int[] array) {
+		int best = array[0];
+		int p = 1;
 
+		for (int i=1; i<array.length; i++) {
+			if ( array[i] == best ) { p++; }
+			else {
+				p--;
+				if ( p == 0) { best = array[i+1]; }
+			}
+		}
+		return best;
+	}
+
+
+	public static void main(String[] args) {
+		int[] array = new int[]{5, 10, 5, 5, 20, 12, 8, 5, 5};
+		System.out.println(findMostOccurrence(array));
 	}
 }
