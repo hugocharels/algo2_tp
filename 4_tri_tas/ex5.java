77c820c431cab2b98d6586ba45b11ef797b77566
Hugo <93338995+hugocharels@users.noreply.github.com>
2023-03-10 12:32:48 +0000

    oups

--- 4_tri_tas/ex5.java
+++ 4_tri_tas/ex5.java
@@ -1,6 +1,25 @@
 class Ex5 {
 
-	public static void main(String[] args) {		
-		System.out.println("pas encore fait");
+	public static void swap(int[] tab, int i, int j) {
+		int tmp = tab[i]; tab[i] = tab[j]; tab[j] = tmp;
+	}
+
+	public static void swim(int[] heap, int idx) {
+		// log n comparaisons atm
+		while (idx > 0 && heap[idx] > heap[idx/2]) {
+			swap(heap, idx, idx/2);
+			idx = idx/2;
+		}
+	}
+
+	public static void insert(int[] heap, int size, int value) {
+		heap[size] = value;
+		swim(heap, size++);
+	}
+
+	public static void main(String[] args) {
+		int[] heap = new int[10];
+		heap[0] = 11; heap[1] = 9; heap[2] = 8; heap[3] = 7; heap[4] = 6; heap[5] = 4;		
+		insert(heap, 6, 10);
 	}
 }
